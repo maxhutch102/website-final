c196936207c6962fd321ea183f110c562a40f20a
Max Hutchison <mhutchi2517@gmail.com>
2026-07-23 17:14:26 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "e0160637-46fe-4cf1-ada5-bfb89d683d02";
 
 const { d1, r2 } = hostingConfig;
 
